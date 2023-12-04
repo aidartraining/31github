bbbe4a1512bd2735df61b4aef3130d924d11e1a1
Aidar Yessembayev <aidarfortraining@gmail.com>
2023-12-04 16:42:20 +0600

    Hash table implementation is finished

---
+++ data_structures/hash_table/hash_table.py
@@ -0,0 +1,25 @@
+class HashTable:
+    def __init__(self, size):
+        self.data = [None] * size
+
+    def set(self, key, value):
+        for idx, item in enumerate(self.data):
+            if item is not None and item[0] == key:
+                self.data[idx] = [key, value]
+                return
+
+        self.data.append([key, value])
+
+    def get(self, key):
+        for item in self.data:
+            if item is not None and item[0] == key:
+                return item[1]
+
+        return None
+
+    def __str__(self):
+        for item in self.data:
+            if item is not None:
+                print(item)
+
+        return ''
---
+++ data_structures/hash_table/main.py
@@ -0,0 +1,20 @@
+from data_structures.hash_table.hash_table import HashTable
+
+
+def main():
+    hash_table = HashTable(10)
+
+    hash_table.set('aidar', 27)
+    hash_table.set('barys', 25)
+    hash_table.set('kairat', 29)
+    hash_table.set('kanat', 23)
+    hash_table.set('aidos', 24)
+
+    hash_table.set('aidos', 28)
+    hash_table.set('aidar', 22)
+
+    print(hash_table)
+
+
+if __name__ == "__main__":
+    main()
