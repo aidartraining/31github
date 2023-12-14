2c981d3372f0046adacfcf0b063d1bcd7531f107
Aidar Yessembayev <aidarfortraining@gmail.com>
2023-12-14 17:57:35 +0600

    Small reformatting

--- data_structures/linked_list/node_dll.py
+++ data_structures/linked_list/node_dll.py
@@ -1,3 +1,6 @@
 class NodeDLL:
     def __init__(self, size):
         pass
+
+    def __str__(self):
+        return ''
--- data_structures/linked_list/node_ll.py
+++ data_structures/linked_list/node_ll.py
@@ -2,3 +2,5 @@ class NodeLL:
     def __init__(self, size):
         pass
 
+    def __str__(self):
+        return ''
